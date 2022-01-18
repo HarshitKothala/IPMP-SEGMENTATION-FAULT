93d0d88b6e55837cc127ecf6e6d89a4bfdc54933
HarshitKothala <76590604+HarshitKothala@users.noreply.github.com>
2022-01-18 20:16:24 +0530

    Add files via upload

---
+++ union.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr1[]={1,2,3,4,5};
+    int arr2[]={4,5,6,7,8};
+
+    int m= sizeof(arr1)/sizeof(arr1[0]);
+    int n= sizeof(arr2)/sizeof(arr2[0]);
+
+   
+    int i=0;
+    int j=0;
+    while (i<m && j<n)
+    {
+        if(arr1[i]<arr2[j])
+        {
+            cout<<arr1[i]<<" ";
+            i=i+1;
+        }
+        else if(arr1[i]>arr2[j])
+        {
+            cout<<arr2[j]<<" ";
+            j=j+1;
+        }
+        else
+        {
+            cout<<arr1[i]<<" ";
+            i=i+1;
+            j=j+1;
+        }
+    }
+    while(i<m)
+    {
+        cout<<arr1[i]<<" ";
+        i=i+1;
+    }
+    while(j<n)
+    {
+        cout<<arr2[j]<<" ";
+        j=j+1;
+    }
+}
---
+++ unique.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,5,4,4,3,5};
+    int size= sizeof(arr)/sizeof(arr[0]);
+    
+
+}
